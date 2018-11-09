46498c27419a79505419382a6ce15facad275f07
coocloud <apple19903@gmail.com>
2018-11-09 15:24:06 +0200

    use process port if present

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('./server/server');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 4000
 
-let server = app.listen(4000, () => {
+let server = app.listen(PORT, () => {
   console.log(`Listening to port ${server.address().port}`);
   console.log(`GraphQL address ${server.address().port}/graphql`);
 });
